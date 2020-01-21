830294d5325bcd12a8d5535f6105522b464c0772
haneefnb <fromaneef@gmail.com>
2020-01-21 08:42:19 +0530

    Sysout added

--- springbootapi/src/main/java/com/morg/springbootapi/SpringbootapiApplication.java
+++ springbootapi/src/main/java/com/morg/springbootapi/SpringbootapiApplication.java
@@ -8,6 +8,7 @@ public class SpringbootapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootapiApplication.class, args);
+		System.out.println("---DOne----");
 	}
 
 }
